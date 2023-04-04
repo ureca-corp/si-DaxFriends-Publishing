71a088cee5ab0219eda4c5eef5356df1558bce95
schatzen <gomieti@naver.com>
2023-04-04 15:04:16 +0900

    Update: Logo에 링크 걸기

--- src/common/components/Appbar/element/components/Logo.tsx
+++ src/common/components/Appbar/element/components/Logo.tsx
@@ -2,12 +2,13 @@ import { MediaQueries } from "@/common/theme/screen";
 import { css } from "@emotion/react";
 import Image from "next/image";
 import ImgLogo from "@/assets/logo/ic-logo.png";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div css={st.logo}>
+    <Link href="/" css={st.logo}>
       <Image fill src={ImgLogo} alt="logo" />
-    </div>
+    </Link>
   );
 };
 
@@ -16,6 +17,7 @@ const st = {
     position: relative;
     width: 16.66vh;
     aspect-ratio: 1;
+    cursor: pointer;
 
     @media ${MediaQueries.lg} {
       width: 18.22vw;
