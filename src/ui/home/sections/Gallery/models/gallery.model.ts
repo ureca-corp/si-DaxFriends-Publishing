00661042b8ee7751393f94a43f51057b08107c24
schatzen <gomieti@naver.com>
2023-04-04 16:59:23 +0900

    Update: gallery 카드 추가

--- src/ui/home/sections/Gallery/models/gallery.model.ts
+++ src/ui/home/sections/Gallery/models/gallery.model.ts
@@ -1,54 +1,58 @@
+import Image1 from "@/assets/pc/gallery/card/1.jpg";
+import Image2 from "@/assets/pc/gallery/card/2.jpg";
+import Image3 from "@/assets/pc/gallery/card/3.jpg";
+import Image4 from "@/assets/pc/gallery/card/4.png";
+import Image5 from "@/assets/pc/gallery/card/5.jpg";
+import Image6 from "@/assets/pc/gallery/card/6.jpg";
+import Image7 from "@/assets/pc/gallery/card/7.jpg";
+import Image8 from "@/assets/pc/gallery/card/8.png";
+import Image9 from "@/assets/pc/gallery/card/9.jpg";
+import Image10 from "@/assets/pc/gallery/card/10.jpg";
+import Image11 from "@/assets/pc/gallery/card/11.png";
+
 export const GalleryModels = [
   {
-    img: "",
+    img: Image1,
     name: "123345",
   },
   {
-    img: "",
+    img: Image2,
     name: "123345",
   },
   {
-    img: "",
+    img: Image3,
     name: "123345",
   },
   {
-    img: "",
+    img: Image4,
     name: "123345",
   },
   {
-    img: "",
+    img: Image5,
     name: "123345",
   },
   {
-    img: "",
+    img: Image6,
     name: "123345",
   },
   {
-    img: "",
+    img: Image7,
     name: "123345",
   },
   {
-    img: "",
+    img: Image8,
     name: "123345",
   },
   {
-    img: "",
+    img: Image9,
     name: "123345",
   },
   {
-    img: "",
+    img: Image10,
     name: "123345",
   },
   {
-    img: "",
-    name: "123345",
-  },
-  {
-    img: "",
-    name: "123345",
-  },
-  {
-    img: "",
+    img: Image11,
     name: "123345",
   },
 ];
